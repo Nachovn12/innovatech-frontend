7cef89f7cd959acd72bd4bdb510435107be6abca
Nachovn12 <ig.valeria@duocuc.cl>
2026-06-30 21:59:27 -0400

    Update frontend URLs with AWS ECS Public IPs

--- src/componentes/CrudAdmin/FormDespacho.jsx
+++ src/componentes/CrudAdmin/FormDespacho.jsx
@@ -25,7 +25,7 @@ export const FormDespacho = ({ venta, onClose }) => {
 
     try {
       await axios.put(
-        `http://192.168.30/api/v1/ventas/${venta.idVenta}`,
+        `http://54.237.224.201:8083/api/v1/ventas/${venta.idVenta}`,
         jsonDataSales,
         {
           headers:{
@@ -34,7 +34,7 @@ export const FormDespacho = ({ venta, onClose }) => {
       }
         }
       );
-      await axios.post("http://192.168.320/api/v1/despachos", jsonData, {
+      await axios.post("http://54.162.6.237:8081/api/v1/despachos", jsonData, {
         headers:{
           'Content-Type': 'application/json',
           'Accept': 'application/json'
